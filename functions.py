ce3473a39743cabd5ba792d30c62534946800c9e
bandameediakshaya332-png <bandameediakshaya332@gmail.com>
2025-12-12 19:49:18 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+FILE_NAME = "expenses.json"
+
+def load_data():
+    if not os.path.exists(FILE_NAME):
+        return []
+    try:
+        with open(FILE_NAME, "r") as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        return []
+
+def save_data(data):
+    with open(FILE_NAME, "w") as file:
+        json.dump(data, file, indent=4)
